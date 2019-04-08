c0ba0bfe113d25c65c7757cd75416d68b581d4cf
Yunsena <45133269+Yunsena266@users.noreply.github.com>
2019-04-08 22:58:07 +0300

    Add files via upload

---
+++ 27 set.py
@@ -0,0 +1,9 @@
+# introduction to the set
+clothes={'skirt','socks', 'coat', 'gloves', 'skirt'}
+print(clothes)
+if 'coat' in clothes:
+    print("you already have a coat");
+else:
+    print("Good purchase!")
+
+
