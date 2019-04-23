586bf7fcc1a5832354da471876d74213e3d557a0
Yunsena <45133269+Yunsena266@users.noreply.github.com>
2019-04-23 22:05:22 +0300

    Delete RussianSerials Analysis.py

--- RussianSerials Analysis.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-info=pd.read_csv ("RussianSerials.txt")
-print("Size of table",info.shape)
-print(info.describe(include='all'));
-
